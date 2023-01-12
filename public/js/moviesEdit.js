129148fe1546bbe6200deb9725653938f07ac94d
RocioYapura14 <yapura.rocio@outlook.com>
2023-01-12 00:28:45 -0300

    Subiendo TP Manipulacion de Elementos JS

---
+++ public/js/moviesEdit.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector('h1.titulo');
+let body = document.querySelector('body');
+let section = document.querySelector('section');
+
+h1.classList.add('titulo');
+
+section.classList.add('fondoTransparente');
+body.classList.add('fondoCRUD');
+
